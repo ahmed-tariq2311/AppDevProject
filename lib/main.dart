2cac20f5288870af0fa714dbfbba187b43fa0b21
ahmed-tariq2311 <124722114+ahmed-tariq2311@users.noreply.github.com>
2023-12-31 01:23:36 +0500

    final push

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:creditrack/Views/welcome_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:creditrack/firebase_options.dart';
+import 'package:creditrack/Utils/firebase_options.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final authenticationProvider = StateProvider<bool>((ref) => false);
